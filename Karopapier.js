6f2473ffa88f730f1d2a713e6d544fd81fb98544
Benedikt Magnus <magnus@magnuscraft.de>
2017-12-02 11:57:58 +0100

    Start-, Komprimierungs- und Verzeichnisskript.

---
+++ Karopapier.js
@@ -0,0 +1,58 @@
+///////////////////
+/////Karopapier////
+//Benedikt Magnus//
+///////////////////
+
+var Debug = false;
+if (process.argv[2] == 'debug')
+  Debug = true;
+
+console.log('\nStarte Karopapier.');
+
+//Komprimiere bei Serverstart CSS und Javascript:
+if (!Debug) //Nur im Debugmodus die Kompression umgehen:
+{
+  var Kompressor = require('./scripts/Kompressor.js');
+  Kompressor.Komprimiere();
+  Kompressor = null;
+}
+
+console.log('Beginne mit dem Laden der Module.');
+
+var express = require('express');
+var app = express();
+var compression = require('compression');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(compression()); //Gzip-Kompression für alle Seiten.
+
+console.log('Module geladen.');
+
+//Mache dem Server alle notwendigen Verzeichnisse und Dateien bekannt:
+var Verzeichnisse = require('./scripts/Verzeichnisse.js');
+Verzeichnisse.Eintragen(app, express, Debug);
+Verzeichnisse = null; //Verzeichnisobjekt wird nicht mehr benötigt.
+
+console.log('Beginne mit Ereignisinitialisierung.');
+
+io.on('connection', function(socket)
+{
+  if (Debug)
+    console.log('Verbindung aufgebaut.');
+
+  socket.on('disconnect', function ()
+    {
+      if (Debug)
+        console.log('Verbindung beendet.');
+    }
+  );
+
+});
+
+console.log('Ereignisse initialisiert.');
+
+http.listen(8031, function()
+{
+  console.log('\nServer erfolgreich gestartet: *:8031');
+});
---
+++ scripts/Kompressor.js
@@ -0,0 +1,151 @@
+//Komprimiert alle Javascript- und CSS-Dateien:
+module.exports = {
+  Komprimiere: function ()
+  {
+    console.log('Starte Komprimierung.');
+
+    var fs = require('fs');
+
+    console.log('Komprimiere Javascript...');
+
+    //////////////
+    //Javascript//
+    //////////////
+
+    var KompressorJS = require('uglify-es');
+
+    KompressorJS.Namenscache = {}; //Cache zum Abgleich aller Funktionsnamen in den JS-Dateien.
+
+    var JSVerzeichnisse = ['./www/scripts'];
+    JSVerzeichnisse.forEach( function (Verzeichnis)
+      {
+        Dateidurchlauf(fs, Verzeichnis, '.js', function (Dateiinhalt)
+          {
+            var Optionen = {
+              warnings: true,
+              compress:
+              {
+                passes: 3, //Anzahl der Durchläufe.
+                toplevel: false, //Funktionen und Variablen niemals entfernen, weil das Vorkommen in anderen Skripten nicht erkannt wird!
+                warnings: true
+              },
+              mangle:
+              {
+                toplevel: true //Alle Funktionen und Variablen mit kurzen Namen bennen. Möglich durch den Cache (s. nameCache).
+              },
+              nameCache: KompressorJS.Namenscache // Ein Namenscache sorgt für die Persistenz von Funktionsnamen zwischen allen Javascriptdateien.
+            };
+          
+            var Ergebnis = KompressorJS.minify(Dateiinhalt, Optionen);
+          
+            Fehlerbehandlung(Ergebnis);
+          
+            return Ergebnis.code;
+          }
+        , true);
+      }
+    );
+
+    KompressorJS = null; //Speicher freigeben.
+
+
+    ///////
+    //CSS//
+    ///////
+
+    console.log('Komprimiere CSS...');
+
+    var KompressorCSS = require('clean-css');
+
+    Dateidurchlauf(fs, './www/css', '.css', function (Dateiinhalt)
+      {
+        var Ergebnis = new KompressorCSS().minify(Dateiinhalt);
+
+        Fehlerbehandlung(Ergebnis);
+
+        return Ergebnis.styles;
+      }
+    );
+
+    KompressorCSS = null;
+
+
+    ////////
+    //HTML//
+    ////////
+
+    console.log('Komprimiere HTML...');
+
+    var KompressorHTML = require('html-minifier');
+
+    var HTMLVerzeichnisse = ['./www/index'];
+    HTMLVerzeichnisse.forEach( function (Verzeichnis)
+      {
+        Dateidurchlauf(fs, Verzeichnis, '.html', function (Dateiinhalt)
+          {
+            var Optionen = {
+              collapseWhitespace: true,
+              minifyCSS: true,
+              minifyJS: true,
+              minifyURLs: true,
+              removeComments: true,
+              removeEmptyAttributes: true,
+              removeRedundantAttributes: true,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: true
+            }
+
+            return KompressorHTML.minify(Dateiinhalt, Optionen);
+          }
+        );
+      }
+    );
+
+    KompressorHTML = null;
+
+    console.log('Komprimierung abgeschlossen.');
+  }
+}
+
+/**
+ * @param {Object} fs 
+ * @param {String} Pfad 
+ * @param {String} Typ 
+ * @param {Function} Prozedur 
+ * @param {Boolean} Doppelt 
+ */
+function Dateidurchlauf (fs, Pfad, Typ, Prozedur, Doppelt = false)
+{
+  var Dateien = fs.readdirSync(Pfad);
+
+  if (Doppelt) Dateien = Dateien.concat(Dateien); //Jede Datei doppelt durchlaufen, um auch über Inhalte der späteren zu wissen!
+
+  for (var i in Dateien)
+  {
+    if (Dateien[i].includes(Typ))
+    {
+      var Dateiinhalt = fs.readFileSync(Pfad + '/' + Dateien[i], 'utf8').toString();
+      fs.writeFileSync(Pfad + '/c/' + Dateien[i], Prozedur(Dateiinhalt), 'utf8');
+    }
+  }
+}
+
+/**
+ * @param {String} Ergebnis 
+ */
+function Fehlerbehandlung (Ergebnis)
+{
+  if (Ergebnis.errors == undefined)
+    Ergebnis.errors = Ergebnis.error;
+
+  if ((Ergebnis.error != undefined) && (Ergebnis.error.length != 0))
+  {
+    console.log('');
+    console.log('Ein Fehler ist aufgetreten!');
+    console.log('Fehlerhafte Datei: ' + Dateien[i]);
+    console.log('');
+    throw Ergebnis.error;
+  }
+  if ((Ergebnis.warnings != undefined) && (Ergebnis.warnings.length != 0))
+    console.log(Ergebnis.warnings);
+}
