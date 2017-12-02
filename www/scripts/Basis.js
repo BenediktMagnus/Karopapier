c829abd8f261364f7bb40851f285a7a8e2164562
Benedikt Magnus <Benedikt.Goldberg@web.de>
2017-12-02 13:37:44 +0100

    Palette mit Bildauswahl.

--- www/scripts/Basis.js
+++ www/scripts/Basis.js
@@ -19,14 +19,37 @@ function Initialisieren ()
         for(let x = 0; x < 40; x++)
         {
             let Punkt = document.createElement('td');
-
+            
             Punkt.id = "Punkt_" + x + ':' + y;
             Punkt.x = x;
             Punkt.y = y;
 
             Reihe.appendChild(Punkt);
+
+            Punkt.onclick = PunktKlick;
         }
     }
 
 
+}
+
+function PunktKlick ()
+{
+    console.log(this.offsetLeft + ' : ' + this.offsetTop);
+    //alert(this.id);
+    let Palette = document.getElementById('Palette');
+
+    //Palette zuerst anzeigen, um Größe zu haben:
+    Palette.style.display = 'inline';
+
+    //X- und Y-Wert ermitteln. Wenn die Palette dadurch über den Bildschirm ragte, Wert entsprechend verkleinern:
+    let x = Papier.offsetLeft + this.offsetLeft + this.clientWidth;
+    if (x + Palette.clientWidth > document.body.clientWidth)
+        x = document.body.clientWidth - Palette.clientWidth;
+    let y = Papier.offsetTop + this.offsetTop;
+    if (y + Palette.clientHeight > document.body.clientHeight)
+        y = document.body.clientHeight - Palette.clientHeight;
+
+    Palette.style.left = x + 'px';
+    Palette.style.top = y + 'px';
 }
