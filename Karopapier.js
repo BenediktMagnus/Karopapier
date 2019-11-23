26655fc9c94a4d54291387db14a31f6499ea1e35
Benedikt Magnus <magnus@magnuscraft.de>
2019-11-23 15:18:54 +0100

    Removed minification of HTML, CSS and JS.

--- Karopapier.js
+++ Karopapier.js
@@ -1,22 +1,3 @@
-///////////////////
-/////Karopapier////
-//Benedikt Magnus//
-///////////////////
-
-var Debug = false;
-if (process.argv[2] == 'debug')
-  Debug = true;
-
-console.log('\nStarte Karopapier.');
-
-//Komprimiere bei Serverstart CSS und Javascript:
-if (!Debug) //Nur im Debugmodus die Kompression umgehen:
-{
-  var Kompressor = require('./scripts/Kompressor.js');
-  Kompressor.Komprimiere();
-  Kompressor = null;
-}
-
 console.log('Beginne mit dem Laden der Module.');
 
 var express = require('express');
@@ -25,14 +6,12 @@ var compression = require('compression');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(compression()); //Gzip-Kompression für alle Seiten.
+app.use(compression()); // Gzip-Kompression für alle Seiten.
 
 console.log('Module geladen.');
 
-//Mache dem Server alle notwendigen Verzeichnisse und Dateien bekannt:
-var Verzeichnisse = require('./scripts/Verzeichnisse.js');
-Verzeichnisse.Eintragen(app, express, Debug);
-Verzeichnisse = null; //Verzeichnisobjekt wird nicht mehr benötigt.
+// Mache dem Server alle notwendigen Verzeichnisse und Dateien bekannt:
+app.use('/', express.static('./www/'));
 
 var Kartenverwaltung = require('./scripts/Kartenverwaltung.js');
 Kartenverwaltung.Initialisieren();
