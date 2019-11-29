36aadad11b7722b1e3535ad919eb37abe32853a0
Benedikt Magnus <magnus@magnuscraft.de>
2019-11-29 13:49:10 +0100

    SQL for creating the SQLite database

---
+++ data/karopapier.sql
@@ -0,0 +1,41 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE `user` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	TEXT NOT NULL UNIQUE,
+	`passwordHash`	TEXT NOT NULL,
+	`sessionId`	TEXT,
+	`isAdmin`	INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `map` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	TEXT NOT NULL UNIQUE,
+	`isActive`	INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `mapEntryAnonymous` (
+	`mapId`	INTEGER NOT NULL,
+	`quantity`	INTEGER NOT NULL,
+	`x`	INTEGER NOT NULL,
+	`y`	INTEGER NOT NULL,
+	`content`	INTEGER NOT NULL,
+	FOREIGN KEY(`mapId`) REFERENCES `map`(`id`)
+);
+
+CREATE TABLE `mapEntryUser` (
+	`mapId`	INTEGER NOT NULL,
+	`userId`	INTEGER NOT NULL,
+	`x`	INTEGER NOT NULL,
+	`y`	INTEGER NOT NULL,
+	`content`	INTEGER NOT NULL,
+	FOREIGN KEY(`mapId`) REFERENCES `map`(`id`),
+	FOREIGN KEY(`userId`) REFERENCES `user`(`id`)
+);
+
+CREATE UNIQUE INDEX `userName` ON `user` (`name`);
+CREATE INDEX `userSessionId` ON `user` (`sessionId`);
+CREATE INDEX `mapEntryAnonymousCoordinates` ON `mapEntryAnonymous` (`mapId`, `x`, `y`);
+CREATE INDEX `mapEntryUserCoordinates` ON `mapEntryUser` (`mapId`, `x`, `y`);
+
+COMMIT;
