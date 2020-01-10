f865d4268bf7516b4cd0ed639b3ed6a88f992ab7
Benedikt Magnus <magnus@magnuscraft.de>
2020-01-10 17:46:58 +0100

    Table descriptions in SQL file

--- data/karopapier.sql
+++ data/karopapier.sql
@@ -1,5 +1,6 @@
 BEGIN TRANSACTION;
 
+-- The classical user table with their credentials:
 CREATE TABLE `user` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`name`	TEXT NOT NULL UNIQUE,
@@ -7,6 +8,7 @@ CREATE TABLE `user` (
 	`isAdmin`	INTEGER NOT NULL DEFAULT 0
 );
 
+-- Sessions are used to identify users after their login:
 CREATE TABLE `session` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`userId`	INTEGER NOT NULL,
@@ -15,6 +17,7 @@ CREATE TABLE `session` (
 	FOREIGN KEY(`userId`) REFERENCES `user`(`id`),
 );
 
+-- Maps and their meta data:
 CREATE TABLE `map` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`publicIdentifier`	TEXT NOT NULL UNIQUE,
@@ -22,6 +25,7 @@ CREATE TABLE `map` (
 	`isActive`	INTEGER NOT NULL DEFAULT 1
 );
 
+-- The collection of anonymous entries:
 CREATE TABLE `mapEntryAnonymous` (
 	`mapId`	INTEGER NOT NULL,
 	`ip`	TEXT NOT NULL,
@@ -32,6 +36,7 @@ CREATE TABLE `mapEntryAnonymous` (
 	FOREIGN KEY(`contentId`) REFERENCES `content`(`id`)
 );
 
+-- The collection of user entries:
 CREATE TABLE `mapEntryUser` (
 	`mapId`	INTEGER NOT NULL,
 	`userId`	INTEGER NOT NULL,
@@ -43,12 +48,14 @@ CREATE TABLE `mapEntryUser` (
 	FOREIGN KEY(`contentId`) REFERENCES `content`(`id`)
 );
 
+-- The collection of possible contents (tools) for a map tile:
 CREATE TABLE `content` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`publicIdentifier`	TEXT NOT NULL,
 	`name`	TEXT NOT NULL
 );
 
+-- Containts which contents (tools) are allowed in a maps:
 CREATE TABLE `mapContent` (
 	`mapId`	INTEGER NOT NULL,
 	`contentId`	INTEGER NOT NULL,
@@ -56,11 +63,16 @@ CREATE TABLE `mapContent` (
 	FOREIGN KEY(`contentId`) REFERENCES `content`(`id`)
 );
 
+-- Search by user name is important for logins:
 CREATE UNIQUE INDEX `userName` ON `user` (`name`);
+-- Search by map's public identifier is needed for selecting maps via URL:
 CREATE UNIQUE INDEX `mapPublicIdentifier` ON `map` (`publicIdentifier`);
+-- Speeding up tile searches:
 CREATE INDEX `mapEntryAnonymousCoordinates` ON `mapEntryAnonymous` (`mapId`, `x`, `y`);
+-- Speeding up tile searches:
 CREATE INDEX `mapEntryUserCoordinates` ON `mapEntryUser` (`mapId`, `x`, `y`);
 
+-- The default anonymous user, here to go sure the ID is zero and the first entry:
 INSERT INTO
 	`user` (`id`, `name`, `passwordHash`, `isAdmin`)
 VALUES
