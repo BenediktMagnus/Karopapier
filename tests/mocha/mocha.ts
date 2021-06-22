1d9674657a91222675656103bb09008b26f97af9
Benedikt Magnus <magnus@magnuscraft.de>
2021-06-22 11:46:29 +0200

    Configured test and coverage support.

---
+++ tests/mocha/mocha.ts
@@ -0,0 +1,14 @@
+import 'mocha';
+import { strictEqual } from 'assert';
+
+describe('Mocha',
+    function ()
+    {
+        it('is working.',
+            function ()
+            {
+                strictEqual(true, true);
+            }
+        );
+    }
+);
